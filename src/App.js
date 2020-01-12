18331adca83dfc3695eaf18b8070e581d1847fa2
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2020-01-12 18:49:38 -0300

    added react routes and update in home page path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-    <HomePage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       <div className="App">
+         <Route path="/" component={HomePage} exact />
+       </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
