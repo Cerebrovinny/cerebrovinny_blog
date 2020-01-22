0c2de44819b0d370ab3103ecdd288894877911e7
Cerebrovinny <vinny.cardoso96@gmail.com>
2020-01-22 20:33:32 -0300

    added components to navbar and style-components to site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import {
   BrowserRouter as Router,
   Route, Switch,
 } from 'react-router-dom';
+import GlobalStyles from './styles/Global';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
-import NavBar from './NavBar';
+import NavBar from './components/Navbar/NavBar';
 import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
   render() {
     return (
       <Router>
        <div className="App">
-         <NavBar />
+         <NavBar  navbarState={this.state.navbarOpen} 
+          handleNavbar={this.handleNavbar} />
+          <GlobalStyles />
          <div id="page-body">
          <Switch>
          <Route path="/" component={HomePage} exact />
